8e70d02b167d56e33564c87bf7a01084584630c6
Andrew Hudson <andrew@ventureharbour.com>
2022-11-03 09:14:54 +0000

    this?

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -3,12 +3,14 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    outDir: "lib",
     lib: {
       entry: "src/index.ts",
       formats: ["es"],
+      name: "Web Component November",
     },
-    rollupOptions: {
-      external: /^lit/,
-    },
+    // rollupOptions: {
+    //   external: /^lit/
+    // }
   },
 });
