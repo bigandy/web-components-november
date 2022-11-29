8107ffdb46b93b74dd4e9b27692e7ecd03a0c1eb
Andrew Hudson <andrew@ventureharbour.com>
2022-11-29 15:45:19 +0000

    add deviceOrientation

--- src/components/ah-geolocation/index.ts
+++ src/components/ah-geolocation/index.ts
@@ -21,6 +21,13 @@ export class AHGeolocation extends LitElement {
   @state()
   coords?: string;
 
+  @state()
+  deviceOrientation?: {
+    alpha: number | null;
+    beta: number | null;
+    gamma: number | null;
+  } | null = null;
+
   geoFindMe() {
     if (!navigator.geolocation) {
       //   status.textContent =
@@ -44,7 +51,7 @@ export class AHGeolocation extends LitElement {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
 
-    console.log({ position: position.coords });
+    // console.log({ position: position.coords });
 
     this.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
 
@@ -57,7 +64,30 @@ export class AHGeolocation extends LitElement {
     console.error(e);
   }
 
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    window.addEventListener(
+      "deviceorientation",
+      (orientation) => this.handleOrientation(orientation)
+    );
+  }
+
+  handleOrientation(event: DeviceOrientationEvent) {
+    const alpha = event.alpha;
+    const beta = event.beta;
+    const gamma = event.gamma;
+
+    this.deviceOrientation = {
+      alpha,
+      beta,
+      gamma,
+    };
+  }
+
   render() {
+    console.log(this.deviceOrientation);
+
     return html`<ah-button @click=${this.geoFindMe}>
         Show my location</ah-button
       ><br />
@@ -65,7 +95,46 @@ export class AHGeolocation extends LitElement {
         ? html`<a href=${this.href} target="_blank"
             >${this.mapLinkText}</a
           >`
-        : ""} `;
+        : ""}
+      ${this.deviceOrientation !== null
+        ? html`
+            <table>
+              <thead>
+                <tr>
+                  <th>Alpha</th>
+                  <th>Beta</th>
+                  <th>Gamma</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                <tr>
+                  <td>
+                    ${this.deviceOrientation?.alpha
+                      ? Math.floor(
+                          this.deviceOrientation?.alpha
+                        )
+                      : ""}
+                  </td>
+                  <td>
+                    ${this.deviceOrientation?.beta
+                      ? Math.floor(
+                          this.deviceOrientation?.beta
+                        )
+                      : ""}
+                  </td>
+                  <td>
+                    ${this.deviceOrientation?.gamma
+                      ? Math.floor(
+                          this.deviceOrientation?.gamma
+                        )
+                      : ""}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          `
+        : html`<p>No deviceOrientation</p>`} `;
   }
 
   static styles = css``;
